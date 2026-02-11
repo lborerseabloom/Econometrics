e621885162aab86542c7bd490d723b7063875ff8
Liam Borer Seabloom <ligma1248@gmail.com>
2026-02-11 14:58:34 -0600

    further alc tax data imports

---
+++ Main Project/alc_imports.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(stringr)
+library(purrr)
+library(readxl)
+
+files <- list.files("data/MN Liquor", full.names = TRUE)
+
+# bind all files together
+alc_tax <- map_dfr(files, function(file_path){
+  # use the two separate read methods in readxl
+  if (grepl(".xlsx$", file_path, ignore.case = TRUE)) {
+    read_xlsx(file_path)
+  } else {
+    read_xls(file_path)
+  }
+})
+# drop useless columns
+alc_tax <- alc_tax[,1:3] 
+alc_tax <- rename(alc_tax, 
+                  year=YEAR, 
+                  county=`COUNTY LIQUOR SALES & TAX (ON & OFF-SALE)`, 
+                  sales=`LIQUOR SALES`)|>
+  drop_na(year)|>
+  filter(! county %in% c("NON-MINNESOTA CO","MN UNKNOWN COUNTY"))
+# alc_tax should now have years*counties of rows, 16*87=1392 in this case
+# since 2024 isn't out yet we have to impute here
